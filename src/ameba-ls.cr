48a7da6a387c372b65fdcaa62d4ef24cef4f7fbf
wuestenfee <v9zoaitjs@mozmail.com>
2025-05-15 04:39:32 +0200

    Add basic option parser

--- src/ameba-ls.cr
+++ src/ameba-ls.cr
@@ -1,7 +1,26 @@
 require "larimar/api/provider_server"
 require "ameba"
+require "option_parser"
 
 require "./ameba-ls/provider"
+
+OptionParser.parse do |parser|
+  parser.banner = "language server for the ameba linter for crystal lang"
+
+  parser.on "-v", "--version", "Show version" do
+    puts "version: " + {{ env("APP_VERSION") || "dev" }}
+    exit
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+  parser.invalid_option do |option_flag|
+    STDERR.puts "ERROR: #{option_flag} is not a valid option."
+    STDERR.puts parser
+    exit 1
+  end
+end
 
 server = Larimar::Server.new(STDIN, STDOUT)
 
